47dee04d59681c83ae3cd3c4ad84e585e4101abe
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-05 00:32:07 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,9 @@
 class Dog
   attr_accessor :name, :breed, :age
 
-  def initialize(attributes)
-    attributes.each do |key, value|
-      self.send("#{key.to_s}=", value)
-    end
+  def initialize(name=nil, breed=nil, age=nil)
+    @name = name
+    @breed = breed
+    @age = age
   end
 end
