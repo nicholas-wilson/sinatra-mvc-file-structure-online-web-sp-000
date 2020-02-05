5691572f2007adadddcb0e4610478c880cd3a1f6
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-05 00:29:26 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,9 @@
-class Dog < ActiveRecord::Base
-  
+class Dog
+  attr_accessor :name, :breed, :age
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send("#{key.to_s}=", value)
+    end
+  end
 end
